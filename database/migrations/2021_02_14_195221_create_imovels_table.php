3e07fff25b9de0ae63ac2a44c7cc5fa18868d77b
Rodrigo Costa <rodrigocosta@outlook.it>
2021-02-14 16:12:52 -0400

    start do projeto

---
+++ database/migrations/2021_02_14_195221_create_imovels_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImovelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('imovels', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->string('foto');
+            $table->string('endereco');
+            $table->string('cep');
+            $table->string('cidade');
+            $table->string('estado');
+            $table->text('descricao');
+            $table->double('valor', 15,8)->default(0.0);
+            $table->enum('status', array('d', 'a'))->default('d');/*d = disponível e a = alugado*/
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('imovels');
+    }
+}
